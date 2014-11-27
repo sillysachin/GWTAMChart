9ba2dd544d48816912e8e86dc0258646e1511c4e
Sachin Shekhar R <sachin.r.r@gmail.com>
2014-11-28 00:57:27 +0530

    Fixed bad checkin and cleaned up todo.txt

--- src/com/amcharts/json/JsFunction.java
+++ src/com/amcharts/json/JsFunction.java
@@ -1,8 +1,6 @@
 package com.amcharts.json;
 
 import com.amcharts.api.IsFunction;
-import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.core.client.JsonUtils;
 
 public class JsFunction implements IsFunction
 {
@@ -18,20 +16,6 @@ public class JsFunction implements IsFunction
 	public void setFunctionString( String functionString )
 	{
 		this.functionString = functionString;
-	}
-
-	public static JavaScriptObject getJSInstance( JsFunction jsFunction )
-	{
-		JavaScriptObject jsFunctionJSO = JsonUtils.unsafeEval( jsFunction
-				.getFunctionString() );
-		return jsFunctionJSO;
-	}
-
-	public static JsFunction getInstance( JavaScriptObject jsFunctionJSO )
-	{
-		JsFunction jsFunction = new JsFunction();
-		jsFunction.setFunctionString( jsFunctionJSO.toString() );
-		return jsFunction;
 	}
 
 	@Override
